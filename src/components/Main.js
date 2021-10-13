e5f22dd3b9b5efa90817bd1f437bca75e142ac51
gedunlap <g.e.dunlap90@gmail.com>
2021-10-13 15:16:46 -0400

    state in main

--- src/components/Main.js
+++ src/components/Main.js
@@ -5,11 +5,34 @@ import Index from '../pages/Index'
 import Show from '../pages/Show'
 
 export default function Main(props) {
+    const [workouts, setWorkouts] = useState(null)
+
+    const URL = "https://rockandrow-backend.herokuapp.com/workouts"
+
+    const getWorkouts = async () => {
+        const response = await fetch(URL)
+        const data = await response.json()
+        setWorkouts(data)
+    }
+
+    const createWorkouts = async (workout) => {
+        await fetch(URL, {
+            method: "post",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(workout)
+        })
+        getWorkouts()
+    }
+
+    useEffect(() => getWorkouts(), [])
+
     return (
         <main>
             <Switch>
                 <Route exact path='/'>
-                    <Index />
+                    <Index workouts={workouts} createWorkouts={createWorkouts} />
                 </Route>
                 <Route
                     path='/workouts/:id'
