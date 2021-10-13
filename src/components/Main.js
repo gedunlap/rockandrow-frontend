e8981677feeaa4a6eee1aa9748f4db9c27888600
gedunlap <g.e.dunlap90@gmail.com>
2021-10-13 15:02:27 -0400

    main routes

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import { useEffect, useState } from 'react'
+import { Route, Switch } from 'react-router-dom'
+import Index from '../pages/Index'
+import Show from '../pages/Show'
 
-export default function Main() {
+export default function Main(props) {
     return (
-        <div>
-            Main
-        </div>
+        <main>
+            <Switch>
+                <Route exact path='/'>
+                    <Index />
+                </Route>
+                <Route
+                    path='/workouts/:id'
+                    render={(rp) => (<Show {...rp} />)}
+                />
+            </Switch>
+        </main>
     )
 }
