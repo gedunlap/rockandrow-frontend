630ad37c32f8686f8ae3150aa814ed5f7fc94721
gedunlap <g.e.dunlap90@gmail.com>
2021-10-13 17:40:33 -0400

    working on header

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,10 +4,11 @@ import { Link } from 'react-router-dom'
 export default function Header() {
     return (
         <nav className="nav">
-            <Link to='/'>
+            <Link to='/' style={{textDecoration: "none"}}>
                 <div>Home</div>
             </Link>
-            <Link to='/list'>
+            <img src="https://i.imgur.com/hSdfdNu.png" alt="" />
+            <Link to='/list' style={{textDecoration: "none"}}>
                 <div>Rock N Row</div>
             </Link>
         </nav>
