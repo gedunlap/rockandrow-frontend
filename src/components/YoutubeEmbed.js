96d4bc1ef8d798a0e0e32bb6a0fa8d16915589b4
gedunlap <g.e.dunlap90@gmail.com>
2021-10-14 15:15:00 -0400

    iframe tweak

--- src/components/YoutubeEmbed.js
+++ src/components/YoutubeEmbed.js
@@ -4,7 +4,7 @@ import PropTypes from "prop-types";
 const YoutubeEmbed = ({ embedId }) => (
   <div className="video-responsive">
     <div class="container">
-        <iframe class="responsive-iframe" src="https://www.youtube.com/embed/ZN0J6qKCIrI">
+        <iframe class="responsive-iframe" src="https://www.youtube.com/embed/ZN0J6qKCIrI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
         </iframe>
     </div>
   </div>
